0a839719698bc0a1f40ef0a62eed47c6aac09738
Angel Pichardo <007172588@coyote.csusb.edu>
2025-06-09 21:10:09 -0700

    Added Ollama LLM for parsing
    Picked the ollama model to have as the LLM that will answer questions about the user query from the scrapping information.

--- LLMscrap/main.py
+++ LLMscrap/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from search import search_with_ollama
 
 st.title("Web Scrap")
 url = st.text_input("Enter an url: ")
@@ -7,4 +8,23 @@ url = st.text_input("Enter an url: ")
 if st.button("Scrape Site"):
     st.write("Scraping......")
     result = scrape_website(url)
-    print(result)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+    
+    with st.expander("View DOM content..."):
+        st.text_area("DOM Content", cleaned_content, height = 300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to search from content.")
+
+    if st.button("Search Content"):
+        if parse_description:
+            st.write("Searching....")
+            
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = search_with_ollama(dom_chunks, parse_description)
+            st.write(result)
+
+    
